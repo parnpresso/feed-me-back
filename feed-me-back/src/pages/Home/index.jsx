c699cd9694f58a8d8aba78607bbf4d0b90f04ff4
Parn Boonyamanee <parnpresso@gmail.com>
2020-02-05 16:18:23 +0700

    💄 Update style of Homepage :crown:

--- feed-me-back/src/pages/Home/index.jsx
+++ feed-me-back/src/pages/Home/index.jsx
@@ -1,16 +1,67 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { useHistory } from 'react-router-dom';
 
 import ParnImage from './images/parn.png';
 
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+`;
+
+const Title = styled.h1`
+  font-size: 1.8em;
+`;
+
+const Image = styled.img`
+  width: 40%;
+  border-radius: 50%;
+  margin: 60px;
+`;
+
+const Content = styled.p`
+  padding-left: 10%;
+  padding-right: 10%;
+  text-align: center;
+  color: #535c68;
+`;
+
+const Button = styled.button`
+  margin-top: 20px;
+  margin-bottom: 20%;
+  padding: 0.5em 4em 0.5em 4em;
+  border: none;
+  border-radius: 0.3em;
+  background: #FED462;
+  font-size: 1.2em;
+  font-weight: 600;
+  letter-spacing: 1px;
+  color: #202020;
+`;
+
 const HomePage = () => {
+  const history = useHistory();
+
+  const goToRatingPage = () => history.push('/rating');
+
   return (
-    <div>
-      <h1>Welcome to Parn's 360 feedback!</h1>
-      <img width="50%" src={ParnImage} />
-      <p>สวัสดีครัช 🤩<br />ป่านรบกวนเวลาซัก 5 นาทีให้ Feedback ป่านหน่อยนะ <br />ป่านจะได้เรียนรู้ข้อผิดพลาดแล้วนำไปปรับปรุงในครั้งต่อๆไปครัชชช 😃</p>
-      <Link to="/rating">Start rating</Link>
-    </div>
+    <Container>
+      <Title>Parn's 360 feedback!</Title>
+      <Image src={ParnImage} />
+      <Content>
+        สวัสดีครัช <span role="img" aria-label="Excited face">🤩</span><br />
+        ป่านรบกวนเวลาซัก 5 นาที<br />
+        ให้ Feedback ป่านหน่อยนะ <br />
+      </Content>
+      <Content>
+        ป่านจะได้เรียนรู้ข้อผิดพลาดแล้วนำไปปรับปรุงในครั้งต่อๆไปครัชชช <span role="img" aria-label="Smiled face">😃</span>
+      </Content>
+      <Button onClick={() => goToRatingPage()}>START</Button>
+    </Container>
   );
 }
 export default HomePage;
--- feed-me-back/src/pages/Success/index.jsx
+++ feed-me-back/src/pages/Success/index.jsx
@@ -5,7 +5,7 @@ const SuccessPage = () => {
   return (
     <div>
       <h1>Success</h1>
-      <p>Feedback เป็นสิ่งที่ีคุณค่าสำหรับป่านมาก ขอบคุณมากครับ ❤️</p>
+      <p>Feedback เป็นสิ่งที่ีคุณค่าสำหรับป่านมาก ขอบคุณมากครับ <span role="img" aria-label="Red heart">❤️</span></p>
       <p>We all need people who will give us feedback. That’s how we improve - Bill Gates</p>
       <Link to="/">Homepage</Link>
     </div>
