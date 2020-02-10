894a49e232e0aa3ee82ea971bfdc87abebf50c87
Parn Boonyamanee <parnpresso@gmail.com>
2020-02-10 12:04:01 +0700

    🎨 Add all question :crown:

---
+++ feed-me-back/src/constants/questions.jsx
@@ -0,0 +1,139 @@
+export default [
+  {
+    order: 1,
+    title: 'Codebase',
+    description: 'Focused on growth & learning our stack, best practices, and codebase.',
+    category: 'Technical',
+  },
+  {
+    order: 2,
+    title: 'Problems',
+    description: 'Works on scoped problems with some guidance, contributing meaningfully.',
+    category: 'Technical',
+  },
+  {
+    order: 3,
+    title: 'Coding',
+    description: 'Writes clean code and tests, iterating based on feedback.',
+    category: 'Technical',
+  },
+  {
+    order: 4,
+    title: 'Technical',
+    description: 'Participates in code reviews and technical design.',
+    category: 'Technical',
+  },
+  {
+    order: 5,
+    title: 'On call',
+    description: 'May participate in on-call rotation, if applicable for their domain.',
+    category: 'Technical',
+  },
+  {
+    order: 6,
+    title: 'Commitment',
+    description: 'Commits to & completes tasks within expected time frame, holding themselves accountable. Building estimation skills.',
+    category: 'Execution',
+  },
+  {
+    order: 7,
+    title: 'Help me please 😂',
+    description: 'Asks for help to get unblocked when necessary.',
+    category: 'Execution',
+  },
+  {
+    order: 8,
+    title: 'Learning',
+    description: 'Learning tools and resources.',
+    category: 'Execution',
+  },
+  {
+    order: 9,
+    title: 'Communicate',
+    description: 'Learning to work collaboratively on a team and communicate in meetings.',
+    category: 'Collaboration & Communication',
+  },
+  {
+    order: 10,
+    title: 'Work status',
+    description: 'Effectively communicates work status to teammates and manager.',
+    category: 'Collaboration & Communication',
+  },
+  {
+    order: 11,
+    title: 'Ask for help',
+    description: 'Proactively asks questions and reaches out for help when stuck.',
+    category: 'Collaboration & Communication',
+  },
+  {
+    order: 12,
+    title: 'Voice',
+    description: 'Voices concerns or need for clarification to their manager.',
+    category: 'Collaboration & Communication',
+  },
+  {
+    order: 13,
+    title: 'Feedback and learn',
+    description: 'Accepts feedback graciously and learns from experience.',
+    category: 'Collaboration & Communication',
+  },
+  {
+    order: 14,
+    title: 'Team impact',
+    description: 'Has project/team-level impact.',
+    category: 'Influence',
+  },
+  {
+    order: 15,
+    title: 'Pair',
+    description: 'Pairs to gain knowledge.',
+    category: 'Influence',
+  },
+  {
+    order: 16,
+    title: 'Team',
+    description: 'Represents their team well to others in the company.',
+    category: 'Influence',
+  },
+  {
+    order: 17,
+    title: 'Hiring',
+    description: 'May participate in hiring.',
+    category: 'Influence',
+  },
+  {
+    order: 18,
+    title: 'Odds',
+    description: 'Learns and exhibits Odds core behaviors, treating others with respect, honoring commitments to the team, seeking out & integrating feedback.',
+    category: 'Maturity',
+  },
+  {
+    order: 19,
+    title: 'Goal',
+    description: 'Objectively evaluates whether they\'ve met their goals.',
+    category: 'Maturity',
+  },
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
